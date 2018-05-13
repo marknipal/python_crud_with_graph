adcf413dedf8271f1faae160c4c8bc57b47dfc39
Mark Nipal <omnimarkbanzai@gmail.com>
2018-05-13 21:34:58 +0800

    added pseudocode for project

--- main.py
+++ main.py
@@ -1,3 +1,44 @@
-#! python2
-print "hellow world!;
+#! python3
+import turtle
 
+#menu
+def rootMenu():
+    print("Welcome! What do you want to do?")
+    print("[0] Add Student")
+    #createStudent()
+    #ASK Student Name
+    #Ask If User want to store Grade
+    #IF YES GOTO storeGrade()
+
+    print("[1] Store Grade on Existing Student")
+    #storeGrade()
+    #ASK IF FOR Discussions Forum or Final Project
+    #IF for Discussion Forum or Final Proj
+    #ask for DF Report Grade:
+    #ask for Project Grade
+    #ask for Final Exam Grade
+
+    print("[2] View Grade of Students for each Assesment")
+    #viewGrades()
+    #ASK IF FOR Discussions Forum or Final Project
+    #view all student grades in Discussions Forum
+
+    print("[3] View Students with below Average Grade for each Assesment")
+    #viewBelowAveGrades()
+    #ASK IF FOR Discussions Forum or Final Project
+    #view all student grades in Discussions Forum
+
+    print ("[4] View Report of Students Total Grade for the Semester")
+    #viewSemesterGrade()
+    #view barchart of total grades by totalling:
+
+    #DF REPORT 20%
+    #Project 30%
+    #Final Exam 50%
+
+    #on all assesments
+
+    print("[5] View Report of Students with below Average Grade for the Semester")
+    #viewSemesterLowGrades()
+    #view report of student that are below average for the semester
+    
